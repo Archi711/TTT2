28940749a5e6f42d70646d3fdc3e370698592e60
Archi711 <mlody711@o2.pl>
2019-11-14 19:23:28 +0100

    First commit (too late, I know) base project, landing page and basic login  , basic server operations (auth, room management)

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Landing from './Landing';
+import socket from '../api';
+
+const App = (props) => {
+  const [state, setState] = useState({auth : false});
+  if(state.auth !== true) return <Landing authorized={(a) => a ? setState({auth : true}) : false} />;
+  else {
+    return `You, named: ${socket.name} are logged in room ${socket.room}`;
+  }
+}
+
+export default App;
